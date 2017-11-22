64b72f352bca7c5e7494f499ebdb4df090a57757
thelostscientist <zeyad.deeb@icloud.com>
2017-11-22 16:44:02 -0600

    adding quarter defs

--- nrfcalendar/NRFCalendar.py
+++ nrfcalendar/NRFCalendar.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from datetime import datetime
+
 class NRFCalendar(object):
     """ Main class for retail calendar"""
     def __init__(self):
@@ -7,3 +12,15 @@ class NRFCalendar(object):
         self.QUARTER_4 = 4
         self.FOUR_WEEK_MONTHS = [2, 5, 8, 11]
         self.FIVE_WEEK_MONTHS = [3, 6, 9, 12]
+
+    def end_of_year(year):
+      year_end = Date.new((year + 1), 1, -1)
+      wday = (year_end.wday + 1) % 7
+      if wday > 3:
+          year_end += 7 - wday
+      else:
+        year_end -= wday
+    return year_end
+
+    def start_of_year(year):
+      return end_of_year(year - 1) + 1
