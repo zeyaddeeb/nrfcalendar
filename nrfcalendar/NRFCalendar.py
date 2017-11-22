91bd45193daeaf8b5936428c1a20f39141f1b387
thelostscientist <zeyad.deeb@icloud.com>
2017-11-22 16:35:02 -0600

    adding quarter defs

--- nrfcalendar/NRFCalendar.py
+++ nrfcalendar/NRFCalendar.py
@@ -1,1 +1,9 @@
-__version__ = '0.1'
+class NRFCalendar(object):
+    """ Main class for retail calendar"""
+    def __init__(self):
+        self.QUARTER_1 = 1
+        self.QUARTER_2 = 2
+        self.QUARTER_3 = 3
+        self.QUARTER_4 = 4
+        self.FOUR_WEEK_MONTHS = [2, 5, 8, 11]
+        self.FIVE_WEEK_MONTHS = [3, 6, 9, 12]
