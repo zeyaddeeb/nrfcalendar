9fff0bef16250d69f418e540073cbc1d279cfb6d
thelostscientist <zeyad.deeb@icloud.com>
2017-12-02 14:42:04 -0600

    update

--- nrfcalendar/NRFCalender.py
+++ nrfcalendar/NRFCalender.py
@@ -1,378 +1,75 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from datetime import date, timedelta
+from datetime import datetime
 
+class NRFCalendar(object):
+    """ Main class for retail calendar"""
+    def __init__(self):
+        self.QUARTER_1 = 1
+        self.QUARTER_2 = 2
+        self.QUARTER_3 = 3
+        self.QUARTER_4 = 4
+        self.FOUR_WEEK_MONTHS = [2, 5, 8, 11]
+        self.FIVE_WEEK_MONTHS = [3, 6, 9, 12]
 
-def cumsum(alist):
-    """ A generator that return a cumulative running sum of elements in a list.
-
-    :param alist: list of numeric.
-    :yield: the current running sum.
-    """
-    tot = 0
-    for e in alist:
-        tot += e
-        yield tot
-
-
-class CalendarImplError(Exception):
-    """ Raise this error when a property in the base class, e.g. BaseDate, is not overridden or implemented.
-    """
-    pass
-
-
-class BaseDate(object):
-    """
-    The base calendar class for polymorphism and shared property implementations
-    """
-
-    def __init__(self, mdate):
-        """ Initialize a date in regular calendar with the given datetime.date object.
-
-        :param mdate: the given datetime.date object.
-        :return:
-        """
-        self.__date = mdate
-
-    @property
-    def year(self):
-        return self.__date.year
-
-    @property
-    def year_start_date(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def year_end_date(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def is_current_year(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def is_previous_year(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def quarter(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def quarter_start_date(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def quarter_end_date(self):
-        raise CalendarImplError("Not implemented")
-        pass
-
-    @property
-    def year_num_of_days(self):
-        """ Number of days in the calendar year containing this date instance.
-        """
-        diff = self.year_end_date - self.year_start_date
-        return diff.days + 1
-
-    @property
-    def quarter_num_of_days(self):
-        """ Number of days in the calendar quarter containing this date instance.
-        """
-        diff = self.quarter_end_date - self.quarter_start_date
-        return diff.days + 1
-
-    @property
-    def year_string(self):
-        """ Year string with both years.
-        E.g.: 2015 - 2016 for fiscal year 2016.
-        """
-        return "%d - %d" % (self.year_start_date.year, self.year_end_date.year)
-
-    @property
-    def year_dates_string(self):
-        """ Year string with formatted starting and ending dates.
-        E.g.: 2016 (26-JUL-2015 - 30-JUL-2016)
-        """
-        start_string = self.year_start_date.strftime("%d-%b-%Y")
-        end_string = self.year_end_date.strftime("%d-%b-%Y")
-        return "%d (%s - %s)" % (self.year, start_string, end_string)
-
-    @property
-    def quarter_string(self):
-        """ Quarter string.
-        E.g.: Quarter 3
-        """
-        return "Quarter " + str(self.quarter)
-
-    @property
-    def quarter_dates_string(self):
-        """ Quarter string with formatted starting and ending dates.
-        E.g.: 2016 (26-JUL-2015 - 30-JUL-2016)
-        """
-        start_string = self.quarter_start_date.strftime("%d-%b-%Y")
-        end_string = self.quarter_end_date.strftime("%d-%b-%Y")
-        return "%d (%s - %s)" % (self.quarter, start_string, end_string)
-
-
-class RegularDate(BaseDate):
-    """
-    This utility class converts a given datetime.date instance into a REGULAR calendar's date instance with
-    different pre-computed attributes of interest such as quarter starting date for that date, etc.
-    """
-
-    _QUARTER_NUM_TO_DATE = { 1: ((1, 1), (3, 31)),
-                             2: ((4, 1), (6, 30)),
-                             3: ((7, 1), (9, 30)),
-                             4: ((10, 1), (12, 31))}
-
-    def __init__(self, mdate, today=None):
-        """ Initialize a date in regular calendar with the given datetime.date object.
-
-        :param mdate: the given datetime.date object.
-        :param today: default is the current date (today), if not specified.
-        :return:
-        """
-        self._date = mdate
-        if not today:
-            self._today = date.today()
+    def end_of_year(year):
+        y_end = datetime((year + 1), 1, 31)
+        w_day = (year_end.weekday() + 1) % 7
+        if w_day > 3:
+            y_end += 7 - w_day
         else:
-            self._today = today
+            y_end -= w_day
+        return y_end
 
-        self.year_start = date(self._date.year, 1, 1)
-        self.year_end = date(self._date.year, 12, 31)
+    def start_of_year(year):
+        return end_of_year(year - 1) + 1
 
-    @property
-    def year(self):
-        """ Return the calendar year of the given date.
-        """
-        return self._date.year
-
-    @property
-    def year_start_date(self):
-        """ Start date of the calendar year containing this date instance.
-        """
-        return self.year_start
-
-    @property
-    def year_end_date(self):
-        """ End date of the calendar year containing this date instance.
-        """
-        return self.year_end
-
-    @property
-    def is_current_year(self):
-        """ Is this instance in the current calendar year, if today is as given?
-        """
-        return True if (self._today.year == self.year) else False
-
-    @property
-    def is_previous_year(self):
-        """ Is the given date in the previous calendar year, if today is as given?
-        """
-        return True if (self._today.year - 1 == self.year) else False
-
-    @property
-    def quarter(self):
-        """ Find the quarter number for the given date.
-
-        Quarter is based on month (every three months), which is one-based in self._date.
-        :return: Quarter number for the input date.
-        """
-        zero_based_month = self._date.month - 1
-        quarter_num = zero_based_month / 3 + 1
-        return quarter_num
-
-    @property
-    def quarter_start_date(self):
-        """ Find the starting date of the quarter that contains the given date.
-        """
-        start_date = self._QUARTER_NUM_TO_DATE[self.quarter][0]
-        return date(self.year, *start_date)
-
-    @property
-    def quarter_end_date(self):
-        """ Find the ending date of the quarter that contains the given date.
-        """
-        end_date = self._QUARTER_NUM_TO_DATE[self.quarter][1]
-        return date(self.year, *end_date)
-
-    @property
-    def year_string(self):
-        """ String of the current year.
-
-        For regular calendar, override base implementation to print one year.
-        """
-        return str(self.year)
-
-    pass
-
-
-class RetailDate(BaseDate):
-    """
-    This utility class converts a given datetime.date instance into a RETAIL calendar's date instance with
-    different pre-computed attributes of interest such as quarter starting date for that date, etc.
-
-    More on Retail calendar: https://en.wikipedia.org/wiki/4%E2%80%934%E2%80%935_calendar
-    """
-    # Each fiscal year starts on August 1st.
-    # Retail calendar's end date is the last Saturday of the month at fiscal year end.
-    FISCAL_START_MONTH = 8
-    FISCAL_START_DAY = 1
-    # Weeks in each month: Grouping of 13 weeks in a quarter can be 5-4-4 or 4-4-5.
-    WEEKS_IN_MONTH = [4, 4, 5]*4
-
-    # In the case of 53-week, the extra week is put in the last month. In this case, last quarter is (5, 4, 5)
-    LEAP_MONTH = 12
-
-    def __init__(self, mdate, today=None):
-        """ Initialize a date in retail calendar with the given datetime.date object.
-
-        :param mdate: the given datetime.date object.
-        :param today: default is the current date (today), if not specified.
-        :return:
-        """
-        self._date = mdate
-        if not today:
-            self._today = date.today()
+    def start_of_month(year, merch_month):
+        m_start = start_of_year(year) + int((merch_month - 1) / 3) * 91
+        if merch_month in FOUR_WEEK_MONTHS:
+            m_start = m_start + 28
+        elif merch_month in FIVE_WEEK_MONTHS:
+            m_start = m_start + 63
         else:
-            # Useful when verifying functionality when running on a particular date.
-            self._today = today
+            print("Invalid merch month")
+        return m_start
 
-        self.year_start, self.year_end = RetailDate.get_retail_start_end(self._date)
-
-        self.is_53_week = True if self.year_num_of_days == 53 * 7 else False
-        # Create an instance copy
-        self._weeks_in_month = self.WEEKS_IN_MONTH[:]
-        if self.is_53_week:
-            # Add additional week to the leap month
-            self._weeks_in_month[self.LEAP_MONTH-1] += 1
-
-        self.weeks_in_quarter = [sum(self._weeks_in_month[0:3]),
-                            sum(self._weeks_in_month[3:6]),
-                            sum(self._weeks_in_month[6:9]),
-                            sum(self._weeks_in_month[9:12])]
-        pass
-
-    @staticmethod
-    def get_retail_start_end(mdate):
-        """ Get the retail year's starting and ending dates that contain the given date
-
-        :param mdate: the given date.
-        :return:
-        """
-
-        retail_end = RetailDate.retail_end_by_year(mdate.year)
-
-        if mdate <= retail_end:
-            # mdate is in the current retail year
-            year_start = RetailDate.retail_end_by_year(mdate.year-1) + timedelta(1)
-            year_end = retail_end
+    def end_of_month(year, merch_month):
+        if merch_month == 12:
+            y_end = end_of_year(year)
         else:
-            # mdate is in the next retail year
-            year_start = retail_end + timedelta(1)
-            year_end = RetailDate.retail_end_by_year(mdate.year+1)
+            y_end = start_of_month(year, merch_month + 1) - 1
+        return y_end
 
-        return year_start, year_end
+    def start_of_week(year, month, merch_week):
+        return start_of_month(year, month) + ((merch_week - 1) * 7)
 
-    @staticmethod
-    def retail_end_by_year(year):
-        """ Retail calendar's year end for the given year.
-        Retail calendar's end date is the last Saturday of the month at fiscal year end.
+    def end_of_week(year, month, merch_week):
+        return start_of_month(year, month) + (6 + ((merch_week - 1) * 7))
 
-        :param year:
-        :return:
-        """
-        fiscal_start = date(year, RetailDate.FISCAL_START_MONTH, RetailDate.FISCAL_START_DAY)
-        # if fiscal_start is Sunday, then it's a retail start.
-        # Otherwise, it is the very last Sunday.
-        if fiscal_start.weekday() == 6:
-            retail_start = fiscal_start
+    def start_of_quarter(year, quarter):
+        if quarter == QUARTER_1:
+            q_start =  start_of_month(year, 1)
+        elif quarter == QUARTER_2:
+            q_start = start_of_month(year, 4)
+        elif quarter == QUARTER_3:
+            q_start = start_of_month(year, 7)
+        elif quarter == QUARTER_4:
+            q_start = start_of_month(year, 10)
         else:
-            # If Monday, weekday() == 0, move it back by 1 day
-            # If Saturday, weekday() = 5, move it back by 6 days
-            retail_start = fiscal_start - timedelta(fiscal_start.weekday() + 1)
+            print("Invalid quarter")
+        return q_start
 
-        # Retail year end is simply the day before that.
-        return retail_start-timedelta(1)
-
-    @property
-    def year(self):
-        """ Return the retail year of the given date.
-
-        Retail year is defined as the year number of the retail year end.
-        :return:
-        """
-        return self.year_end.year
-
-    @property
-    def year_start_date(self):
-        """ Start date of the retail year containing this date instance.
-        """
-        return self.year_start
-
-    @property
-    def year_end_date(self):
-        """ End date of the retail year containing this date instance.
-        """
-        return self.year_end
-
-    @property
-    def is_current_year(self):
-        """ Is this instance in the current retail year, if today is as given?
-        """
-        _, today_year_end = self.get_retail_start_end(self._today)
-        return True if (today_year_end.year == self.year) else False
-
-    @property
-    def is_previous_year(self):
-        """ Is the given date in the previous retail year, if today is as given?
-        """
-        _, today_year_end = self.get_retail_start_end(self._today)
-        return True if (today_year_end.year - 1 == self.year) else False
-
-    @property
-    def quarter(self):
-        """ Find the retail quarter number for the given date.
-
-        Quarter is based on month (every three months), which is one-based in self._date.
-        :return: Quarter number for the input date.
-        """
-        num_days = (self._date - self.year_start_date).days
-        week_num = num_days / 7
-
-        week_cumsum = [0]
-        week_cumsum.extend(cumsum(self.weeks_in_quarter))
-        count = 0
-        for count in xrange(1, len(week_cumsum)):
-            if week_cumsum[count-1] <= week_num < week_cumsum[count]:
-                break
-        return count
-
-    @property
-    def quarter_start_date(self):
-        """ Find the starting date of the quarter that contains the given date.
-        """
-        # Find the running total of weeks per quarter
-        week_cumsum = [0]
-        week_cumsum.extend(cumsum(self.weeks_in_quarter))
-        start_date = self.year_start_date + timedelta(week_cumsum[self.quarter-1]*7)
-        return start_date
-
-    @property
-    def quarter_end_date(self):
-        """ Find the ending date of the quarter that contains the given date.
-        """
-        # Find the running total of weeks per quarter
-        week_cumsum = list(cumsum(self.weeks_in_quarter))
-        end_date = self.year_start_date + timedelta(week_cumsum[self.quarter-1]*7-1)
-        return end_date
-
-
-    pass
+    def end_of_quarter(year, quarter):
+        if quarter == QUARTER_1:
+            q_end = start_of_month(year, 3)
+        elif quarter == QUARTER_2:
+            q_end = start_of_month(year, 6)
+        elif quarter == QUARTER_3:
+            q_end = start_of_month(year, 9)
+        elif quarter == QUARTER_4:
+            q_end = start_of_month(year, 12)
+        else:
+            print("Invalid quarter")
+        return q_end
